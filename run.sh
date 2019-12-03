16c823bddbb0ba748dfabad14a20f6faa5eac5b2
Matt Banner <matt@banner.wtf>
2019-12-03 21:08:56 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,11 +15,19 @@ AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?"AWS_ACCESS_KEY_ID missing"}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?"AWS_SECRET_ACCESS_KEY missing"}
 AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:?"AWS_DEFAULT_REGION missing"}
 
-# Create Config Files
-echo "[default]" >> /root/.aws/config
-echo "[default]" >> /root/.aws/credentials
-echo "region = $AWS_DEFAULT_REGION" >> /root/.aws/config
-echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
+# If there is no AWS config, make one
+if [[ ! -e /root/.aws/config ]]; then
+  touch /root/.aws/config
+  echo "[default]" >> /root/.aws/config
+  echo "region = $AWS_DEFAULT_REGION" >> /root/.aws/config
+fi
+
+# If there are no AWS credentials, make some
+if [[ ! -e /root/.aws/credentials ]]; then
+  touch /root/.aws/credentials
+  echo "[default]" >> /root/.aws/credentials
+  echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
+  echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
+fi
 
 exec /usr/sbin/cron -f
